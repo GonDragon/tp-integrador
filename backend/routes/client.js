cf7dd84bfa494e78c48dd4ad2b756a9561597f7f
Gonzalo Romero <gondragon13@gmail.com>
2026-06-02 14:13:57 -0300

    plantillas ejs basicas

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('<h1>Clientes</h1>');
+    res.render('client/datos_cliente');
 });
 
 module.exports = router;
