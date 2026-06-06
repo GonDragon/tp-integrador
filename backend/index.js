5c5b7ffcb57587d8b8c95ea060e1981668d150ab
Gonzalo Romero <gondragon13@gmail.com>
2026-06-06 17:55:57 -0300

    Middleware catalogo

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const { connectDB, sequelize } = require('./config/db');
 require('./models');
 
@@ -18,6 +19,7 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/productos', rutasProductos);
 
--- backend/routes/client.js
+++ backend/routes/client.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
+//Middlewares
+const verificarCliente = (req, res, next) => {
+    const cookieCliente = req.cookies.cliente;
+    const nombreParam = req.query.nombre;
+    const nombreTrim =  nombreParam.trim();
+
+    if (nombreParam && nombreTrim !== '') {
+        res.cookie('cliente', nombreTrim, {
+            httpOnly: true
+        });
+
+        req.cookies.cliente = nombreTrim;
+
+        return next();
+    } else if (nombreParam) {
+        // Eliminar la cookie si el parametro nombre es vacio
+        res.clearCookie('cliente');
+        return res.redirect('/');
+    }
+
+    if (cookieCliente) {
+        return next();
+    }
+
+    return res.redirect('/');
+};
+
+//Rutas
 router.get('/', (req, res) => {
     res.render('client/datos_cliente');
+});
+
+router.get('/catalogo', verificarCliente, (req, res) => {
+    const cliente = req.cookies.cliente || req.query.nombre;
+    res.render('client/catalogo', { cliente });
 });
 
 module.exports = router;
